dc26f175e5c85cd3411274f4e011b47af9823b51
Govnocode4U <konstantinkozoriz1310@gmail.com>
2022-11-05 08:22:53 +0300

    file name changed:
    interface.py -> main_gui.py

--- client/interface/main_interface.py
+++ client/interface/main_interface.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 from time import sleep
 from PyQt5.QtGui import QIcon, QColor, QPainter
-from interface import Ui_MainWindow
+from main_gui import Ui_MainWindow
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -10,9 +10,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    application_1 = MainWindow()
-    application_1.show()
+    MainMenu = MainWindow()
+    MainMenu.show()
 
     sys.exit(app.exec())
