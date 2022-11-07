1c5c2729fc087b18897cb371ac458ee07e16d4d7
Natanus2011 <ngrunskaya1994@gmail.com>
2022-11-07 23:56:12 +0300

    searching game(most pop) by tags

--- client/client.py
+++ client/client.py
@@ -1,5 +1,4 @@
 import socket
-
 import config_network
 
 
@@ -9,8 +8,18 @@ class Client:
         self.client = socket.socket()
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(config_network.SERVER_ADDR)
-        message = self.client.recv(4096).decode('utf-8')
-        print(message)
+        self.recev_mes()
+
+    def recev_mes(self):
+        test_message1 = 'Horror_Online Co-Op_Multiplayer_Psychological Horror_Co-op_VR_Supernatural_' \
+                        'First-Person_Investigation_Dark'
+        test_message2 = 'Exploration_Singleplayer_Robots_Third Person_Beautiful_Horror_BMX_Sniper'
+        test_message3 = 'Multiplayer_Strategy'
+        self.client.send(test_message2.encode('utf-8'))
+        while 1:
+            m = self.client.recv(4096).decode('utf-8')
+            print(m.split(", ")[1][1:-1])
+            break
 
 
 client = Client()
--- server/guesser.py
+++ server/guesser.py
@@ -1,9 +1,13 @@
+import sqlite3
+
 from database import DataBase
 from utils import Game, Tag
 
-
 db = DataBase()
 all_tags = db.get_all_tags()
+
+sqlite_connection = sqlite3.connect('database.db')
+cursor = sqlite_connection.cursor()
 
 
 def search_games(tmp_game: Game) -> list[Game]:
@@ -18,4 +22,66 @@ def close():
     db.disconnect()
 
 
-close()
+def guesser(string):
+    tags = string.split('_')
+    tags.sort(key=compare_tags, reverse=True)
+    print(tags)
+
+    for i in range(len(tags)):
+        tags[i] = compare_tags(tags[i], 0)
+
+    close()
+    return selection(tags)
+
+
+def selection(tags):
+    ans = sort_games(tags)
+    flag = False
+
+    while not flag:
+        try:
+            game_select = "SELECT * from games WHERE id = ?"
+            cursor.execute(game_select, (ans[0],))
+            record = cursor.fetchall()
+            flag = True
+        except:
+            del (tags[-1])
+            ans = sort_games(tags)
+
+    return record
+
+
+def compare_games(a, num=3):
+    rate_select = "SELECT * from games WHERE id = ?"
+    cursor.execute(rate_select, (a,))
+    record = cursor.fetchone()[num]
+
+    return int(record)
+
+
+def compare_tags(a, num=3):
+    rate_select = "SELECT * from tags WHERE tag_name = ?"
+    cursor.execute(rate_select, (a,))
+    record = cursor.fetchone()[num]
+
+    return int(record)
+
+
+def search_one_tag(tags, num):
+    game_select = "SELECT * from game_to_tag WHERE tag_id = ?"
+    cursor.execute(game_select, (tags[num],))
+    games = cursor.fetchall()
+    for i in range(len(games)):
+        games[i] = games[i][0]
+    return games
+
+
+def sort_games(tags):
+    ans = search_one_tag(tags, 0)
+    for i in range(1, len(tags)):
+        temp = search_one_tag(tags, i)
+        ans = list(set(temp) & set(ans))
+
+    ans.sort(key=compare_games, reverse=True)
+
+    return ans
