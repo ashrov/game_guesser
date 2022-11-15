04f5739809c0afce2c4069a80da973a9f7b68912
Anton <aksharov2003@gmail.com>
2022-11-15 19:51:49 +0300

    path configuration modified

--- server/config.py
+++ server/config.py
@@ -1,4 +1,7 @@
 import os
 
 
-DATABASE_NAME = os.path.dirname(__file__) + r"\database.db"
+DATABASE_FILE_NAME = r"\database.db"
+DATABASE_PATH = os.path.dirname(__file__) + DATABASE_FILE_NAME
+
+GAME_SELECTION_SIZE = 3
