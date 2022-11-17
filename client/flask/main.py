63e0917bcc3a812c87e89044c1da36d64e6f1c36
v4n99 <v4n99@mail.ru>
2022-11-17 14:47:07 +0300

    page with chosen games

--- client/flask/main.py
+++ client/flask/main.py
@@ -8,6 +8,7 @@ message = {'intent': 'get_all_tags'}
 tags = client.send_message(message)['all_tags']
 tags = [tag['tag_name'] for tag in tags]
 tags = tags[:40]
+result_games = []
 
 
 @app.route('/', methods=['GET'])
@@ -18,16 +19,12 @@ def get_index():
 @app.route('/', methods=['POST'])
 def post_index():
     chosen_tags = request.form.getlist('checkbox[]')
-    print(chosen_tags)
     message = {'intent': 'get_by_tags',
                'tags': chosen_tags}
-    print(client.send_message(message))
-    return redirect("/result", code=302)
+    result_games = client.send_message(message)["current_games"]
+    result_games = [game for game in result_games]
 
-
-@app.route('/result')
-def result():
-    return "vanya frontend developer"
+    return render_template("games_page.html", games=result_games)
 
 
 if __name__ == '__main__':
